ccc9c878130db65fce772c1d81b0b2c592e2a783
Justo Garrido <jgarrido@iies.es>
2020-10-14 11:08:30 +0200

    Tarea 00 Completada

---
+++ 20201009 Tarea 00 Escritura de dos letras en CS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _20201009_caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] letra = genera('Q');
+            imprime(letra);
+            Console.WriteLine("\n");
+            letra = genera('M');
+            imprime(letra);
+        }
+
+        static public char[,] genera(char letra) {
+            char[,] matr = new char[7,7];
+            switch(letra) {
+                case 'Q':
+                    matr = new char[7,7] {  {' ','*','*','*','*','*',' '},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ','*',' '},
+                                            {' ','*','*','*','*',' ','*'}
+                    };
+                    break;
+                case 'M':
+                    matr = new char[7,7] {  {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*','*',' ',' ',' ','*','*'},
+                                            {'*',' ','*',' ','*',' ','*'},
+                                            {'*',' ',' ','*',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'},
+                                            {'*',' ',' ',' ',' ',' ','*'}
+                    };
+                    break;
+               // default:
+
+            }
+            return matr;
+        }
+
+        static public void imprime(char[,] matr) {
+            for (int fila=0; fila < 7; fila++) {
+                for (int colu=0; colu<7; colu++) {
+                    Console.Write(matr[fila,colu]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
